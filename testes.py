719d019f289a62f8510de8fb9e5f7688492364c1
MR-MARCOSs <marcosdudu001@gmail.com>
2024-06-13 23:47:11 -0300

    new wait function

--- testes.py
+++ testes.py
@@ -5,22 +5,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
 # Configuração do webdriver
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.get('https://web.whatsapp.com')
 
 # Tempo para escanear o QR Code e abrir o WhatsApp Web
-print("Por favor, escaneie o QR Code do WhatsApp Web.")
-WebDriverWait(driver, 60).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "canvas[aria-label='Scan me!']"))
-)
+input("asd")
 
 # Espera até que a página seja carregada completamente
-WebDriverWait(driver, 60).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "div[data-testid='chat-list']"))
-)
 
 # Seleciona o contato desejado
-contact_name = "Nome do Contato"  # Substitua pelo nome do contato
+contact_name = "COMANDOS - JS"  # Substitua pelo nome do contato
 contact = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, f"//span[@title='{contact_name}']"))
 )
@@ -40,7 +34,7 @@ while True:
 
         # Espera até que o botão do menu de contexto esteja presente
         context_menu_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, ".//span[@data-testid='menu']"))
+            EC.presence_of_element_located((By.XPATH, ".//div[@aria-label='Menu de contexto']"))
         )
         context_menu_button.click()
 
@@ -51,7 +45,9 @@ while True:
         forward_option.click()
 
         # Marca todas as checkboxes
-        checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox' and @aria-label='teste']")
+        checkboxes = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//input[@type='checkbox' and @aria-label='teste']"))
+        )
         for checkbox in checkboxes:
             checkbox.click()
         
@@ -60,6 +56,5 @@ while True:
     except Exception as e:
         print("Erro:", e)
         break
-
 # Fecha o driver após a execução
-driver.quit()
+
